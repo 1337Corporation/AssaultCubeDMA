ffc8f518dcd14cb9933ec10697b143cf3ad471b2
kaveO <alban.billiette@gmail.com>
2025-07-20 15:15:58 +0200

    + | Added found Offsets in main .h for modulable code

--- AssaultCubeDMA/AssaultCubeDMA.hpp
+++ AssaultCubeDMA/AssaultCubeDMA.hpp
@@ -1,3 +1,12 @@
 #pragma once 
 
 constexpr auto PROCESS_NAME = "ac_client.exe";
+
+namespace Offsets
+{
+	constexpr size_t Health			= 0xEC;
+	constexpr size_t MachineGun		= 0x140;
+	constexpr size_t Pistol			= 0x12C;
+
+	constexpr size_t PlayerCameraX	= 0x34;
+}
