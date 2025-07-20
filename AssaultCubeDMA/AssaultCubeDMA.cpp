b801f0fec0178c9928e5389a61d50ce996146d6a
kaveO <alban.billiette@gmail.com>
2025-07-20 19:52:25 +0200

    + | Added CameraX offset + Using Offsets from header

--- AssaultCubeDMA/AssaultCubeDMA.cpp
+++ AssaultCubeDMA/AssaultCubeDMA.cpp
@@ -28,10 +28,16 @@ int main()
     size_t health = 3333333;
     size_t ammo = 42;
 
+    float cameraX;
+
     while (1)
     {
-        TargetProcess.Write(LocalPlayerPtr + 0xEC, &health, sizeof(health));
-        TargetProcess.Write(LocalPlayerPtr + 0x140, &ammo, sizeof(ammo));
+        TargetProcess.Write(LocalPlayerPtr + Offsets::Health, &health, sizeof(health));
+        TargetProcess.Write(LocalPlayerPtr + Offsets::MachineGun, &ammo, sizeof(ammo));
+        TargetProcess.Write(LocalPlayerPtr + Offsets::Pistol, &ammo, sizeof(ammo));
 
+        TargetProcess.Read(LocalPlayerPtr + Offsets::PlayerCameraX, &cameraX, sizeof(cameraX));
+
+        std::cout << "\rPlayer CameraX -> " << cameraX << std::flush;
     }
 }
