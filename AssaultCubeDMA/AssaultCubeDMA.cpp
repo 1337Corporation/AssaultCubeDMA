32b8de76e9e706f518a2f4c587261a277a40c8ae
kaveO <alban.billiette@gmail.com>
2025-07-21 23:03:10 +0200

    + | Added moveCursor function for better visibility in console

--- AssaultCubeDMA/AssaultCubeDMA.cpp
+++ AssaultCubeDMA/AssaultCubeDMA.cpp
@@ -2,6 +2,13 @@
 #include "AssaultCubeDMA.hpp"
 #include <iostream>
 #include <thread>
+#include <windows.h>
+
+void moveCursor(int x, int y)
+{
+    COORD coord = { static_cast<SHORT>(x), static_cast<SHORT>(y) };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
 
 int main()
 {
@@ -47,8 +54,21 @@ int main()
         TargetProcess.Write(LocalPlayerPtr + Offsets::Guns::PistolAmmo, &ammo, sizeof(ammo));
         TargetProcess.Write(LocalPlayerPtr + Offsets::Guns::MachineGunAmmo, &ammo, sizeof(ammo));
 
-        TargetProcess.Read(LocalPlayerPtr + Offsets::PlayerCameraX, &cameraX, sizeof(cameraX));
-        TargetProcess.Read(LocalPlayerPtr + Offsets::PlayerCameraY, &cameraY, sizeof(cameraY));
-        std::cout << "\rPlayer CameraX -> " << cameraX << " | Player CameraY -> " << cameraY << std::flush;
+
+        // Reading Camera Values
+        TargetProcess.Read(LocalPlayerPtr + Offsets::LocalPlayer::PlayerCameraX, &cameraX, sizeof(cameraX));
+        moveCursor(0, 1);
+        std::cout << "Player CameraX -> " << cameraX << std::endl;
+        TargetProcess.Read(LocalPlayerPtr + Offsets::LocalPlayer::PlayerCameraY, &cameraY, sizeof(cameraY));
+        moveCursor(0, 2);
+        std::cout << "Player CameraY -> " << cameraY << std::endl;
+
+        
+        // Reading Position Values
+        TargetProcess.Read(LocalPlayerPtr + Offsets::LocalPlayer::PlayerPosZ, &positionZ, sizeof(positionZ));
+        moveCursor(0, 4);
+        std::cout << "Player Position Z -> " << positionZ << std::endl;
+
+        Sleep(100);
     }
 }
