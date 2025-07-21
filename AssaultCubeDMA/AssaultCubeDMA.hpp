f8c5fde221d07d6c2ab68e3854a3d7c1cf1eb288
kaveOO <alban.billiette@gmail.com>
2025-07-21 16:22:11 +0200

    + | Sorted and found more offsets. reverse in progress

--- AssaultCubeDMA/AssaultCubeDMA.hpp
+++ AssaultCubeDMA/AssaultCubeDMA.hpp
@@ -4,11 +4,28 @@ constexpr auto PROCESS_NAME = "ac_client.exe";
 
 namespace Offsets
 {
-	constexpr size_t Health			= 0xEC;
-	constexpr size_t MachineGun		= 0x140;
-	constexpr size_t Pistol			= 0x12C;
-	constexpr size_t Name			= 0x205;
+	namespace LocalPlayer
+	{
+		constexpr size_t Health							= 0xEC;
+		constexpr size_t Armor							= 0xF0;
+		constexpr size_t Name							= 0x205;
 
-	constexpr size_t PlayerCameraX	= 0x34;
-	constexpr size_t PlayerCameraY	= 0x38;
+		constexpr size_t PlayerCameraX					= 0x34;
+		constexpr size_t PlayerCameraY					= 0x38;
+
+		constexpr size_t PlayerPosX						= 0xFF;
+		constexpr size_t PlayerPosY						= 0xFF;
+		constexpr size_t PlayerPosZ						= 0x30;
+	}
+
+	namespace Guns
+	{
+		constexpr size_t PistolAmmo						= 0x12C;
+		constexpr size_t PistolAvailableAmmo			= 0x108;
+
+		constexpr size_t MachineGunAmmo					= 0x140;
+		constexpr size_t MachineGunAvailableAmmo		= 0x11C;
+
+		constexpr size_t GrenadeCount					= 0x144;
+	}
 }
