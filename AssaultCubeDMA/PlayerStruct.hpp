9eba6c5746af27bc2fd08ca9d224d63b70660f21
kaveOO <alban.billiette@gmail.com>
2025-07-24 18:27:19 +0200

    + | Added file with more dumped values + PlayerStruct

--- AssaultCubeDMA/PlayerStruct.hpp
+++ AssaultCubeDMA/PlayerStruct.hpp
@@ -2,8 +2,10 @@
 
 struct PlayerStruct
 {
-	Vec3 	Position;	// 0x4 -> 0xC
-	size_t 	Health;		// 0xEC
-	size_t 	Armour;		// 0xF0
-	char	Name[16];	// 0x205
+	Vec3  	Position;	// 0x4
+	size_t 	Health;    	// 0xEC
+	size_t 	Armour;    	// 0xF0
+	size_t 	Frags;     	// 0x1DC
+	size_t 	Deaths;    	// 0x1E4
+	char  	Name[16];   // 0x205
 };
