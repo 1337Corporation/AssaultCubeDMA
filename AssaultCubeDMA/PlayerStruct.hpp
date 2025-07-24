60235b06a20d46bf46bc5dbbb951719ff1834a8e
kaveOO <alban.billiette@gmail.com>
2025-07-24 15:30:58 +0200

    + | Added basic values in PlayerStruct

--- AssaultCubeDMA/PlayerStruct.hpp
+++ AssaultCubeDMA/PlayerStruct.hpp
@@ -1,6 +1,9 @@
-#include "Helpers.hpp"
+#include "Vectors.hpp"
 
 struct PlayerStruct
 {
-
+	Vec3 	Position;	// 0x4 -> 0xC
+	size_t 	Health;		// 0xEC
+	size_t 	Armour;		// 0xF0
+	char	Name[16];	// 0x205
 };
