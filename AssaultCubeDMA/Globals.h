845cc57d0503d082a443013d91c21230b0c02e27
kaveO <alban.billiette@gmail.com>
2025-07-26 18:54:42 +0200

    + | Added global const Offsets

--- AssaultCubeDMA/Globals.h
+++ AssaultCubeDMA/Globals.h
@@ -2,4 +2,10 @@
 
 constexpr auto PROCESS_NAME = "ac_client.exe";
 
-uintptr_t LocalPlayerOffset = 0x0058AC00;
+constexpr uintptr_t LocalPlayerOffset 		= 0x58AC00;
+constexpr uintptr_t PlayersCountOffset		= 0x58AC0C;
+constexpr uintptr_t PlayersListOffset		= 0x58AC04;
+constexpr uintptr_t ViewMatrixOffset		= 0x57DFD0;
+constexpr uintptr_t CurrentFrameOffset		= 0x57F10C;
+constexpr uintptr_t VisibilityCheckOffset	= 0x58A918;
+
