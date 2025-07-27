d6fc3506aa5d5250013bc309a106ec5669068b50
kaveO <alban.billiette@gmail.com>
2025-07-27 12:41:31 +0200

    + | Added Vec3 operators and Distance calculation function

--- AssaultCubeDMA/Vectors.hpp
+++ AssaultCubeDMA/Vectors.hpp
@@ -30,4 +30,34 @@ struct Vec3
 	float X;
 	float Y;
 	float Z;
+
+	Vec3(float x, float y, float z) : X(x), Y(y), Z(z) { }
+
+	Vec3 operator+(const Vec3 &Other) const
+	{
+		return Vec3(X + Other.X, Y + Other.Y, Z + Other.Z);
+	}
+
+	Vec3 operator-(const Vec3 &Other) const
+	{
+		return Vec3(X - Other.X, Y - Other.Y, Z - Other.Z);
+	}
+
+	Vec3 operator/(float Scalar) const
+	{
+		return Vec3(X / Scalar, Y / Scalar, Z / Scalar);
+	}
+
+	Vec3 operator*(float Scalar) const
+	{
+		return Vec3(X * Scalar, Y * Scalar, Z * Scalar);
+	}
+
+    float Distance(const Vec3& Other) const
+	{
+		float Dx = X - Other.X;
+		float Dy = Y - Other.Y;
+		float Dz = Z - Other.Z;
+		return std::sqrt(Dx * Dx + Dy * Dy + Dz * Dz);
+	}
 };
