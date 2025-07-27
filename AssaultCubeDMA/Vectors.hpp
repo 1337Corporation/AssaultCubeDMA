830d1300fe9043dfeb2833424fed486dd4435bee
kaveO <alban.billiette@gmail.com>
2025-07-27 12:27:00 +0200

    + | Useful functions in Vec2 such as constructor & DistanceTo function

--- AssaultCubeDMA/Vectors.hpp
+++ AssaultCubeDMA/Vectors.hpp
@@ -1,7 +1,19 @@
+#pragma once
+
+#include <cmath>
+
 struct Vec2
 {
 	float X;
 	float Y;
+
+	Vec2() = default;
+	Vec2(float x, float y) : X(x), Y(y) { }
+
+	float DistanceTo(const Vec2 &other) const
+	{
+		return std::sqrt(std::pow(other.X - X, 2) + std::pow(other.Y - Y, 2));
+	}
 };
 
 struct Vec3
