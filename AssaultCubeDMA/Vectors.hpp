d09e65e423c3843e4ef526436922b36cfcd46dbd
kaveO <alban.billiette@gmail.com>
2025-07-31 18:51:41 +0200

    - | Removed unused DEFINES

--- AssaultCubeDMA/Vectors.hpp
+++ AssaultCubeDMA/Vectors.hpp
@@ -3,13 +3,6 @@
 #include <cmath>
 #include <imgui.h>
 
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-
-#define RAD2DEG (180.0f / 3.14159265f)
-#define DEG2RAD (3.14159265358979323846f / 180.0f)
-
 struct Vec2
 {
 	float X;
