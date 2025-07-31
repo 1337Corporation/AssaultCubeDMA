7d84936f6257f69061593ee6a66ab47a9b4ad837
kaveO <alban.billiette@gmail.com>
2025-07-31 17:43:21 +0200

    + | Reworked all the colors, sizes and positions ESP.hpp

--- AssaultCubeDMA/Esp.hpp
+++ AssaultCubeDMA/Esp.hpp
@@ -10,37 +10,44 @@ bool	WorldToScreen(const Vec3 &World, Vec2 &Screen, float *M, int Width, int Hei
 void	DrawESP(const std::vector<Player> &Players);
 void	ComputePlayerBox(Player &Player);
 
-namespace Text
-{
-	constexpr ImU32 WhiteTextColor		= IM_COL32(255, 255, 255, 255);
-}
-
 namespace Colors
 {
-	// Player Boxes
-	constexpr ImU32 BoxVisibleEnnemy	= IM_COL32(255, 0, 0, 255);
-	constexpr ImU32 BoxHiddenEnnemy		= IM_COL32(255, 255, 0, 255);
-	constexpr ImU32 BoxFriendly			= IM_COL32(0, 255, 0, 255);
+	namespace U32
+	{
+		constexpr ImU32		White				= IM_COL32(255, 255, 255, 255);
+		constexpr ImU32 	Green			 	= IM_COL32(0, 255, 0, 255);
+		constexpr ImU32		Yellow				= IM_COL32(255, 255, 0, 255);
+		constexpr ImU32 	Red					= IM_COL32(255, 0, 0, 255);
+	}
 
-	// SnapLine
-	constexpr ImU32 SnapLineColor 		= IM_COL32(255, 255, 255, 255);
-
-	// Player Health
-	constexpr ImU32 HealthColorGood 	= IM_COL32(0, 255, 0, 255);
-	constexpr ImU32 HealthColorMedium	= IM_COL32(255, 255, 0, 255);
-	constexpr ImU32 HealthColorBad		= IM_COL32(255, 0, 0, 255);
+	namespace Vec4
+	{
+		constexpr ImVec4	Yellow				= ImVec4(1.0f, 1.0f, 0.0f, 1.0f);
+		constexpr ImVec4 	Green				= ImVec4(0.0f, 1.0f, 0.0f, 1.0f);
+		constexpr ImVec4	Red					= ImVec4(1.0f, 0.0f, 0.0f, 1.0f);
+	}
 }
 
 namespace Sizes
 {
 	// Player Boxes
-	constexpr float	PlayerBoxRounding	= 0.0f;
-	constexpr float PlayerBoxThickness	= 1.0f;
+	constexpr float		PlayerBoxRounding	= 0.0f;
+	constexpr float 	PlayerBoxThickness	= 1.0f;
 
 	// SnapLine
-	constexpr float SnapLineThickness	= 1.0f;
+	constexpr float 	SnapLineThickness	= 1.0f;
 
 	// Health Bar
-	constexpr float HealthBarWidth		= 3.0f;
-	constexpr float HealthBarOffset		= 6.0f;
+	constexpr float 	HealthBarWidth		= 3.0f;
+	constexpr float 	HealthBarOffset		= 6.0f;
+
+	// Player Hover Popup
+	constexpr ImVec2	HoverWindowSize		= ImVec2(200.0f, 0.0f);
+}
+
+namespace Positions
+{
+	// Player Hover Popup (from top left of screen)
+	constexpr float 	PopupX 				= 50.0f;
+	constexpr float 	PopupY 				= 50.0f;
 }
